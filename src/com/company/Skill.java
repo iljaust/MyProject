b73603fa6f70dcc2d321a0af78b55b4c8ae248b2
Ilya Ustinovs <iljaust7@gmail.com>
2020-07-19 16:48:05 +0100

    Version 1.0

---
+++ src/com/company/Skill.java
@@ -0,0 +1,45 @@
+package com.company;
+
+
+
+public class Skill {
+
+    private long id;
+    private String name;
+
+    public Skill(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Skill() {
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    public long getId() {
+
+        return id;
+    }
+
+    public void setId(long id) {
+
+        this.id = id;
+    }
+
+    @Override
+    public String toString(){
+
+        return id+"."+name;
+    }
+
+}
+
---
+++ src/com/company/SkillRepository.java
@@ -0,0 +1,85 @@
+package com.company;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class SkillRepository {
+    private final String SKILL_FILE_NAME = "skills.txt";
+
+    public List<Skill> getAll(){
+        List<Skill> arr = fromFileToArray();
+        return arr;
+    }
+    private List<Skill> fromFileToArray() {
+        File file = new File(SKILL_FILE_NAME);
+        List <Skill> skills = new ArrayList<>();
+
+        try {
+
+            FileReader fr = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fr);
+            String line = reader.readLine();
+            String[] subStr;
+            int s;
+            while (line != null) {
+                String delimeter = "\\.";
+                subStr = line.split(delimeter);
+                skills.add(stringToSkill(subStr[1],Long.parseLong(subStr[0])));
+                line = reader.readLine();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return skills;
+    }
+
+    public Skill save(Skill skill){
+        List<Skill> skills = fromFileToArray();
+        skills.add(skill);
+        fromArrayToFile(skills);
+        return skill;
+    }
+
+    public void deleteById(Long id){
+        List<Skill> skills = fromFileToArray();
+        skills.stream()
+                .filter(e -> e.getId()!= id)
+                .collect(Collectors.toList());
+        fromArrayToFile(skills);
+    }
+
+    public Skill update(Skill skill){
+        List<Skill> skills = fromFileToArray();
+        skills.removeIf(n ->n.getId() == skill.getId());
+        skills.add(skill);
+        fromArrayToFile(skills);
+        return skill;
+    }
+
+    public Skill getById(Long id) {
+        List <Skill> skill = fromFileToArray();
+        Optional <Skill> skillOpt = skill.stream().filter(s -> s.getId() == id).findFirst();
+        return skillOpt.orElse(null);
+    }
+
+    private void fromArrayToFile(List<Skill> skills) {
+        try(FileWriter writer = new FileWriter(SKILL_FILE_NAME, false)){
+            for(Skill skill : skills) {
+                Long number = (long)(skills.indexOf(skill) + 1);
+                String name = skill.getName();
+                writer.write(number + "." + name + "\n");
+            }
+        }
+        catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+    }
+
+    private Skill stringToSkill(String skill,Long id){
+        return new Skill(id, skill);
+    }
+
+}
